1a17ca59c8c5b1f9c84569d9ec2a47f406f95679
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2023-11-29 16:34:10 +0200

    print_alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+
 i = 'a'
+
 while True:
-    print(i,end='')
+    print(i, end='')
     i = chr(ord(i) + 1)
-# (chr) connvert the unicode to character 
-# (ord) convert the charater in the variable into unicode 
+
     if i > 'z':
         break

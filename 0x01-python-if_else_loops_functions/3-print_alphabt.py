11287729b53cafaf3853fd7b016634e335b559ad
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2023-11-29 17:14:54 +0200

    print and alphabet without e,q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-#i = 'a'
-#for i in range(97,123):
-#    if chr(i) not in ['q','e']:
-#        print("{}".format(chr(i)),end="")
+i = 'a'
+for i in range(97,123):
+    if i != ord('q') and i != ord('e'):
+        print("{}".format(chr(i)),end="")
 
 # Loop through ASCII values for lowercase letters (97 to 122)
-for i in range(97, 123):
-    # Use one print function with string format to print characters without a new line, excluding 'q' and 'e'
-    print("{}".format(chr(i)), end='') if chr(i) not in ['q', 'e'] else None
+#or i in range(97, 123):
+#    # Use one print function with string format to print characters without a new line, excluding 'q' and 'e'
+#    print("{}".format(chr(i)), end='') if chr(i) not in ['q', 'e'] else None

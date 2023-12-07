7465ba54761337ce552b0c08713414fdbce8bb97
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2023-12-07 17:57:23 +0200

    last change in task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+
+
 def square_matrix_simple(matrix=[]):
-    return list(map(lambda submat:list(map(lambda x:x**2,submat)), matrix))
+    # Create a new matrix to store the squared values
+    result_matrix = []
+
+    # Iterate through the rows of the input matrix
+    for row in matrix:
+        # Create a new row with squared values using a list comprehension
+        squared_row = [x**2 for x in row]
+        # Append the squared row to the result matrix
+        result_matrix.append(squared_row)
+
+    return result_matrix
 
 
 if __name__ == "__main__":
@@ -10,6 +22,6 @@ if __name__ == "__main__":
         [4, 5, 6],
         [7, 8, 9]
     ]
-result = square_matrix_simple(matrix)
-print(result)
-print(matrix)
+
+    result = square_matrix_simple(matrix)
+    print(result)

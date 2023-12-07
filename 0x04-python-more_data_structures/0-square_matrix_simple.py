b88ca47f48bf575ca2adeaac44e4b403a43fa7ae
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2023-12-07 17:54:43 +0200

    last change in task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -3,11 +3,13 @@ def square_matrix_simple(matrix=[]):
     return list(map(lambda submat:list(map(lambda x:x**2,submat)), matrix))
 
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
+if __name__ == "__main__":
+    # Example usage:
+    matrix = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+    ]
 result = square_matrix_simple(matrix)
 print(result)
 print(matrix)

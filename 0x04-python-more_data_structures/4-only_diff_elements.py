841ed75f14c7ecc0b44db8d727f19455f744b74b
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2023-12-08 19:31:42 +0200

    task 4

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,17 +1,22 @@
 #!/usr/bin/python3
+
 def only_diff_elements(set_1, set_2):
+    """Return a set of elements present in only one set."""
     set1 = set(set_1)
     set2 = set(set_2)
 
-    unique_to_set1 = set1 - set2
-    unique_to_set2 = set2 - set1
+    only_diff_elements_set = set1.symmetric_difference(set2)
 
-    return unique_to_set1, unique_to_set2
+    return only_diff_elements_set
 
 
 if __name__ == "__main__":
+    # Example sets
     set_1 = {"Python", "C", "Javascript"}
     set_2 = {"Bash", "C", "Ruby", "Perl"}
 
-    differentSentence = only_diff_elements(set_1, set_2)
-    print(sorted(list(differentSentence)))
+    # Get elements present in only one set
+    result_set = only_diff_elements(set_1, set_2)
+
+    # Display the result
+    print(sorted(list(result_set)))

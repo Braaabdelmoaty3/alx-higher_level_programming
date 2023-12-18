bd9efe76125c64a1743da11b1234efc4083f617a
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2023-12-18 19:23:15 +0200

    first task

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+def safe_print_list(my_list=[], x=0):
+    for x in my_list:
+        print(x)
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+try:
+    for x in my_list:
+        print(x,end="")
+except IndexError:
+    print("the number is out of list.")

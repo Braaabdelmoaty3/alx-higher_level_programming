091186e36974659f4e77c96d4772e553e39a3998
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2024-01-17 06:08:21 +0200

    mendatory

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+import marshal
+import types
+
+def print_hidden_names(module):
+    names = [name for name in dir(module) if not name.startswith('__')]
+    names.sort()
+    for name in names:
+        print(name)
+
+def load_compiled_module(file_path):
+    with open(file_path, 'rb') as file:
+        file_content = file.read()
+        code = marshal.loads(file_content[16:])
+        module = types.ModuleType(file_path)
+        exec(code, module.__dict__)
+        return module
+
+if __name__ == "__main__":
+    file_path = "hidden_4.pyc"
+    compiled_module = load_compiled_module(file_path)
+    print_hidden_names(compiled_module)

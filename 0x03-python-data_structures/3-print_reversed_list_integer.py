595aeef6d05f8d923da99ec27547edd472ce05a7
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2024-01-24 13:26:45 +0200

    mendatory

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
-    format_string = "\n".join("{} ".format(item) for item in my_list)
+    format_string = "\n".join("{:d} ".format(item) for item in my_list)
     return format_string
 

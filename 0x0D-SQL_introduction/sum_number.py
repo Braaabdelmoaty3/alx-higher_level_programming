e2e8d2294a181c34f2fcc94f80c016eb7907cfd1
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2024-03-13 20:40:36 +0200

    hello javascript

---
+++ 0x0D-SQL_introduction/sum_number.py
@@ -0,0 +1,11 @@
+import math
+
+def number_sum(n):
+    result = sum(range(1,n+1))
+    print(result)
+    pass    
+
+number_sum(6)
+number_sum(3)
+
+# ana 3awez a3rd the number that will make the sum of that numbers

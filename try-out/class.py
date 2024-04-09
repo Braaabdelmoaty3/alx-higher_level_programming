b125b1ff70b8e1bf806d3085653062d28932d952
braaabdelmoaty3 <braaabdelmoaty3@gmail.com>
2024-04-09 15:55:55 +0200

    objects

---
+++ try-out/class.py
@@ -0,0 +1,17 @@
+class Employee:
+    pass
+emp_1 = Employee()
+emp_2 = Employee()
+
+#print(emp_1)
+#print(emp_2)
+
+emp_1.name = 'ahmed'
+emp_1.last_name = 'yasser'
+emp_1.pay = 5000
+
+emp_2.name = 'mohamed'
+emp_2.last_name = 'ahmed'
+emp_2.pay = 3000
+
+print(emp_1.name)
---
+++ try-out/objects.py
@@ -0,0 +1,6 @@
+a = [1,2,3]
+b = a[:]# by [:] we clone the list and dont touch the list of a
+#print (b)
+print()
+b[1] = 5
+print (a)
